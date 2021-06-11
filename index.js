82261bd106155eb62757bad57f8adb47f3876dea
Silviofacanha <silvio.facanha@gmail.com>
2021-06-10 23:29:02 -0300

    criando primeiro codigo tocando som

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-index.js
+import React, { Component } from "react"
+import { Button, Input, Icon,Dropdown,Card} from "semantic-ui-react"
+import { Link } from "react-router-dom"
+import $ from "jquery"
+import styles from "./Home.scss"
+import Modal from "./Modal.jsx"
+import MakeChannelModal from "./MakeChannelModal.jsx"
+
+class Music extends React.Component {
+constructor(props) {
+super(props);
+this.state = {
+
+play: false,
+pause: true
+
+};
+
+this.url = "http://streaming.tdiradio.com:8000/house.mp3";
+this.audio = new Audio(this.url);
+
+}
+
+play(){
+this.setState({
+play: true,
+pause: false
+});
+console.log(this.audio);
+this.audio.play();
+}
+
+pause(){
+this.setState({ play: false, pause: true });
+this.audio.pause();
+}
+
+render() {
+
+return (
+<div>
+<button onClick={this.play}>Play</button>
+<button onClick={this.pause}>Pause</button>
+</div>
+);
+}
+}
+
+
+export default Music
 
 
 
